58768109f46da859ed5cc33bc237df52a5d178f7
mohammad-shexo <mohammad.ibrahim.shexo@gmail.com>
2025-06-23 04:04:43 +0200

    bedrijven.js is alleen voor admin routes als je bedrijven publiek wilt maak een andere bestand want zo werkt mijn code niet

--- server/routes/bedrijven.js
+++ server/routes/bedrijven.js
@@ -1,45 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const adminAuth = require('../middleware/adminAuth');
 const db = require('../db');
 
-//  Alle bedrijven ophalen (publiek, geen authenticatie nodig)
-router.get('/', (req, res) => {
-  const query = `
-    SELECT 
-      b.*, 
-      GROUP_CONCAT(d.naam SEPARATOR ', ') as dienstverbanden
-    FROM 
-      bedrijven b
-    LEFT JOIN 
-      bedrijf_dienstverbanden bd ON b.bedrijf_id = bd.bedrijf_id
-    LEFT JOIN 
-      dienstverbanden d ON bd.dienstverband_id = d.id
-    WHERE 
-      b.is_verified = TRUE
-    GROUP BY
-      b.bedrijf_id
-    ORDER BY
-      b.naam ASC
-  `;
-
-  db.query(query, (err, results) => {
+//  Alle bedrijven ophalen
+router.get('/bedrijven', adminAuth, (req, res) => {
+  db.query('SELECT * FROM bedrijven', (err, results) => {
     if (err) {
       console.error('Fout bij ophalen bedrijven:', err);
       return res.status(500).json({ message: 'Databasefout' });
     }
-    
-    // Converteer de komma-gescheiden string van dienstverbanden naar een array
-    const bedrijven = results.map(bedrijf => ({
-      ...bedrijf,
-      dienstverbanden: bedrijf.dienstverbanden ? bedrijf.dienstverbanden.split(', ') : []
-    }));
-
-    res.json(bedrijven);
+    res.json(results);
   });
 });
 
-//  Eén specifiek bedrijf ophalen via ID (ook publiek)
-router.get('/:id', (req, res) => {
+//  Eén specifiek bedrijf ophalen via ID
+router.get('/bedrijven/:id', adminAuth, (req, res) => {
   const id = req.params.id;
 
   db.query('SELECT * FROM bedrijven WHERE bedrijf_id = ?', [id], (err, results) => {
